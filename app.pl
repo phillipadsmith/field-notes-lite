c1699a738705c7ded8d6df54fafe2fa53ef48e09
Phillip Smith <ps@phillipadsmith.com>
2015-02-21 12:56:51 -0800

    Simple session-based conversations

--- app.pl
+++ app.pl
@@ -4,6 +4,8 @@ use FindBin;
 use lib 'local/lib/perl5';
 use Data::Dumper;
 use Mojolicious::Lite;
+use Mojo::JSON qw(decode_json encode_json);
+use Mojolicious::Sessions;
 use WWW::Twilio::API;
 use WWW::Twilio::TwiML;
 
@@ -23,18 +25,28 @@ my $ua = Mojo::UserAgent->new;
 
 post '/' => sub {
     my $c = shift;
+    
     my $message  = $c->param('Body');
     my $from  = $c->param('From');
     app->log->info( "Got from Twilio: $message" );
     app->log->info( "From: $from" );
-    app->log->info( Dumper( $c->req ) );
-    my $response = $twilio->POST('SMS/Messages',
+
+    # Figure out the field name
+    my $field = $message;
+
+    # Set a cookie for the conversation
+    if ( !$c->session->{'field'} ) { # Don't have a field yet
+        $c->session({ field => $field }); # Story one in a session
+    }
+    app->log->info( $c->session->{'field'} );
+
+    # Should get CREATED in the ->{'message'}
+    my $response = $twilio->POST('SMS/Messages.json',
                           From => $config->{'twilio_num'},
                           To   => $from,
-                          Body => "Hey, let's have lunch" );
+                          Body => "Your field is $field" );
 
-    app->log->info( Dumper( $response ) );
-    $c->render( text => "$message" . Dumper( $response), status => 200 );
+    $c->render( text => "Received: $message, replied and got: $response->{'message'}", status => 200 );
 };
 
 app->start;
